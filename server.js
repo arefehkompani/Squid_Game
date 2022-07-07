87d2e44fb85c570f49e365bf52f051b54fb73757
Arefeh Kompani <awriik00@gmail.com>
2022-07-07 13:27:02 +0430

    We don't need sever.js, we should run from live server of VS code

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = new require("express");
 
 var app = express();
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
-});
+app.use(express.static(__dirname));
+// app.get('/', function (req, res) {
+//     res.sendFile(__dirname + '/index.html');
+// });
 
 app.listen(8080);
 console.log('Express server started');
