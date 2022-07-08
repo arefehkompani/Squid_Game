0f3bce47f776817af080a1e3f365d58c697c414e
Arefeh Kompani <awriik00@gmail.com>
2022-07-08 10:15:48 +0430

    Now you can play

--- js/main.js
+++ js/main.js
@@ -13,6 +13,10 @@ scene.add( light );
 // Global Variables
 const start_position = 3
 const end_position = -start_position 
+const text = document.querySelector(".text ")
+const TIME_LIMIT = 10 
+let gameStatus = "loading"
+let isLookingBackward = true
 
 function createCube(size, positonX, rotY = 0, color = 0xfbc851){
     const geometry = new THREE.BoxGeometry(size.w, size.h, size.d);
@@ -28,6 +32,10 @@ camera.position.z = 5;
 
 const loader = new THREE.GLTFLoader();
 
+function delay(ms){
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+
 class Doll {
     constructor(){
         loader.load("../model/scene.gltf", (gltf) => {
@@ -40,10 +48,20 @@ class Doll {
 
     lookBackward(){
         gsap.to(this.doll.rotation, {y: -3.15, duration: .45})
+        setTimeout(() => isLookingBackward = true, 150)
     }
 
     lookForward(){
         gsap.to(this.doll.rotation, {y: 0, duration: .45})
+        setTimeout(() => isLookingBackward = false, 450)
+    }
+
+    async start(){
+        this.lookBackward()
+        await delay((Math.random()*1000) + 1000)
+        this.lookForward()
+        await delay((Math.random() * 700) + 700)
+        this.start()
     }
 }
 
@@ -76,13 +94,23 @@ class Player{
 
     stop(){
         // this.playerInfo.velocity = 0
-        gsap.to(this.playerInfo, {
-            velocity: 0, 
-            duration: .1
-        })
+        gsap.to(this.playerInfo, {velocity: 0, duration: .1})
+    }
+
+    check(){
+        if(this.playerInfo.velocity > 0 && !isLookingBackward){
+            text.innerText = "You lose!"
+            gameStatus = "over"
+        }
+        if(this.playerInfo.positionX < end_position + .4){
+            text.innerText = "You win!"
+            gameStatus = "over"
+
+        }
     }
 
     update(){
+        this.check()
         this.playerInfo.positionX -= this.playerInfo.velocity
         this.player.position.x = this.playerInfo.positionX
     }
@@ -90,11 +118,37 @@ class Player{
 const player = new Player()
 
 let doll = new Doll()
-setTimeout(() => {
-    doll.lookBackward()
-}, 1000)
+
+async function init(){
+    await delay(500)
+    text.innerText = "Starting in 3"
+    await delay(500)
+    text.innerText = "Starting in 2"
+    await delay(500)
+    text.innerText = "Starting in 1"
+    await delay(500)
+    text.innerText = "Go"
+    gameStatus = "started"
+    startGame()
+}
+
+function startGame(){
+    let progressBar = createCube({w: 5, h: .1, d: 1}, 0)
+    progressBar.position.y = 3.35
+    gsap.to(progressBar.scale, {x:0, duration: TIME_LIMIT, ease: "none"})
+    doll.start()
+    setTimeout(() => {
+        if(gameStatus != "over"){
+            text.innerText = "You ran out of time!"
+            gameStatus = "over"
+        }
+    }, TIME_LIMIT * 1000)
+}
+
+init()
 
 function animate() {
+    if(gameStatus == "over") return
 	renderer.render( scene, camera );
 	requestAnimationFrame( animate );
     player.update()
@@ -111,6 +165,7 @@ function onWindowResize() {
 }
 
 window.addEventListener('keydown', (e) => {
+    if(gameStatus != "started") return
     if(e.key == "ArrowUp"){
         player.run()
     }
